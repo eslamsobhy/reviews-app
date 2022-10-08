d1feff703950d6713b12f31cf9a871f3ce63c598
Eslam Sobhy <eslamsobhy206@gmail.com>
2022-10-08 16:44:37 +0200

    random reviews button

--- src/Review.js
+++ src/Review.js
@@ -27,6 +27,13 @@ const Review = () => {
       return checkNumber(newIndex);
     });
   };
+
+  const randomPerson = () => {
+    let randomNumber = Math.floor(Math.random() * people.length);
+    if (randomNumber == index) randomNumber += 1;
+    setIndex(checkNumber(randomNumber));
+  };
+
   return (
     <article className="review">
       <div className="img-container">
@@ -46,7 +53,9 @@ const Review = () => {
           <FaChevronRight />
         </button>
       </div>
-      <button className="random-btn">surprise me</button>
+      <button className="random-btn" onClick={randomPerson}>
+        surprise me
+      </button>
     </article>
   );
 };
